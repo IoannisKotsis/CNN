d6a51a6991edb79e1d45d825c46eb7ab7d92b2b4
IoannisKotsis <john01ko18@gmail.com>
2025-11-13 13:10:03 +0100

    updated version

--- Pretrained_Models.py
+++ Pretrained_Models.py
@@ -205,9 +205,10 @@ for name, p in model.named_parameters():
 
 model.to(device)
 
+optimizer=optim.Adam([])
 optimizer=optim.Adam([
-    {'params': [p for name,p in model.named_parameters() if name.startswith(('layer4'))], 'lr': 1e-5},
-    {'params': [p for name,p in model.named_parameters() if name.startswith(('fc'))], 'lr': 1e-4},
+    {'params': [p for name,p in model.named_parameters() if name.startswith('layer4','fc')], 'lr': 1e-4},
+    #{'params': [p for name,p in model.named_parameters() if name.startswith('fc')], 'lr': 1e-4},
 ])
 
 criterion=nn.CrossEntropyLoss()
